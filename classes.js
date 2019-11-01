44a21db2fb5a4925608aed88f1d7ca434b76b25e
uaqro <uaqro@protonmail.com>
2019-11-01 12:41:21 -0600

    first commit with basic files

---
+++ classes.js
@@ -0,0 +1,18 @@
+class Player {
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+        this.width = 50;
+        this.height = 50;
+
+
+    }
+}
+class Field {
+    constructor(){
+        this.x = 0;
+        this.y = 0;
+        this.width = canvas.width;
+        this.height = canvas.height;
+    }
+}
---
+++ main.js
@@ -0,0 +1,25 @@
+
+// ** VARIABLES **
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+let interval;
+let frames;
+
+// ** GAME FUNCTIONS **
+
+function checkColitions{
+
+}
+function 
+
+// ** FRAMES & UPDATES **
+
+function update(){
+
+}
+function startGame(){
+
+}
+function gameOver(){
+
+}
