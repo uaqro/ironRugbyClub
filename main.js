7f4cefcb92dbb7eda0313704388f2d7cd2475d2b
uaqro <uaqro@protonmail.com>
2019-11-04 15:30:27 -0600

    first commit. Player working and rivals following the target

--- main.js
+++ main.js
@@ -1,25 +1,97 @@
 
 // ** VARIABLES **
-let canvas = document.getElementById('canvas');
+let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
 let interval;
-let frames;
+let frames = 0;
+let rivals = [];
+let field = new gameField();
+let player = new Player();
+let score = 0;
+let level = 0;
 
 // ** GAME FUNCTIONS **
 
-function checkColitions{
+function clearCanvas(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+}
+
+function gameOver(){
+    clearInterval(interval)
+}
+
+function checkColitions(){
 
 }
-function 
+
+function createRivals(){
+    for(i=0; i<(8+(level*1,5));i++){
+        rivals.push(new Rival())
+    }
+}
+function drawRivals(){
+    rivals.forEach((e, i) => {
+        e.changePosition()
+        e.draw()
+    })
+}
+function moveRivals(){
+    rivals.forEach(e => e.move())
+}
+function animatePlayer(){
+    if (frames%5==0){
+        player.animate++
+        rivals.forEach(e => e.animate++)
+    }
+}
+function drawScore(){
+    ctx.fillStyle = 'white';
+    ctx.font = '25px Arial'
+    ctx.fillText(`Score: ${score}`, 50, 30)
+}
+
+// MONTAR LISTENER PARA RECOGER EL HEADER UNA VEZ INICIADO EL JUEGO
+
+document.onkeydown = e => {
+    switch (e.keyCode) {
+        case 37:
+            player.moveLeft()
+            return
+        case 39:
+            player.moveRight()
+            return
+        case 40:
+            player.moveBack()
+            return
+        case 38:
+            player.moveUp()
+            return
+        case 16:
+            player.turbo()
+    }  
+  }
 
 // ** FRAMES & UPDATES **
 
 function update(){
-
+    frames++
+    clearCanvas()
+    field.draw()
+    drawRivals()
+    animatePlayer()
+    player.x += player.vx
+    player.y += player.vy
+    player.changePosition()
+    player.draw()
+    moveRivals()
+    drawScore()
 }
-function startGame(){
 
+function startGame(){ 
+    createRivals()
 }
-function gameOver(){
 
-}
+interval = setInterval(update, 1000/60)
+createRivals()
+
+
